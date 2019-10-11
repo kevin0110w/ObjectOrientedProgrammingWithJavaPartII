c1ea458cd8d0aa0b4969249d12f450de6d1a545d
kevin0110w <kevin0110w@gmail.com>
2019-10-11 19:16:15 +0100

    string builder

--- week12-week12_48.StringBuilder/src/Main.java
+++ week12-week12_48.StringBuilder/src/Main.java
@@ -6,7 +6,25 @@ public class Main {
         System.out.println(build(t));
     }
 
-    public static String build(int[] t) {
-        return "{\n}\n";
+    public static String build(int[] t) {       
+        StringBuilder builder = new StringBuilder();
+        builder.append("{ ");
+        
+        for (int i = 0; i < t.length; i++) {
+            // if 4 elements have been appended, add a new line
+            if (i % 4 == 0) {
+                builder.append("\n");
+            }
+            // otherwise append the element
+            builder.append(" " + t[i]);
+            
+            // append a comma after each element is added except the last one.
+            if (i != t.length - 1) {
+                builder.append(",");
+            }
+        }
+        builder.append("\n");
+        builder.append("}");
+        return builder.toString();
     }
 }
